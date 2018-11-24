4e2432b8dd640de07bbe9c4253419cec8a5ea4de
daphtya <davidpanjaitan1@gmail.com>
2018-11-24 16:53:10 +0700

    flask app prototype

--- app.py
+++ app.py
@@ -1,19 +1,43 @@
 import flask as fl
 import pickle
-from sklearn import MLPClassifier
+from sklearn.neural_network import MLPClassifier
 
 app = fl.Flask(__name__)
 
-with open('modelbagus.sav', 'wb') as filemodel:
+with open('modelbagus.sav', 'rb') as filemodel:
 	predictor = pickle.load(filemodel)
+
+def preprocessdata(data):
+	arr = []
+	for key in data:
+		if data[key] =="":
+			arr.append(float("NaN"))
+		else:
+			arr.append(float(data[key]))
+	return arr
+
+def prediction_info(pred):
+	if pred == 0:
+		return "No heart disease"
+	else:
+		return "You got type {} heart disease with 60% certainty".format(pred)
 
 
 @app.route('/')
 def main_menu():
-	return "Welcome to my world!"
+	return fl.render_template('index.html')
 
-@app.route('/prediction', method=['POST'])
+@app.route('/prediction', methods=['POST'])
 def show_result():
-	data = fl.request.values
-	
-	
+	data = fl.request.form	
+	prediction = predictor.predict([preprocessdata(data)])
+	return str(prediction_info(prediction[0]))
+
+@app.route('/test')
+def show_test():
+	prediction = predictor.predict([[1, 1, 1, 1, 1, 1, 1, 1, 1, 1]])
+	return str(prediction_info(prediction[0]))
+
+
+if __name__=='__main__':
+	app.run(debug=True)
